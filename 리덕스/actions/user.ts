3dfa415db21f1646c45e8ca6903a589c273fc342
kyomin <kim031504@naver.com>
2021-11-07 02:35:35 +0900

    리덕스 - redux-thunk 타이핑

--- 리덕스/actions/user.ts
+++ 리덕스/actions/user.ts
@@ -1,3 +1,5 @@
+import { addPost } from './post';
+
 export const LOGIN_REQUEST = 'LOGIN_REQUEST' as const;
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS' as const;
 export const LOGIN_FAILURE = 'LOGIN_FAILURE' as const;
@@ -5,25 +7,72 @@ export const LOGOUT = 'LOGOUT' as const;
 
 export interface LoginRequestAction {
 	type: typeof LOGIN_REQUEST;
-	data: { id: string; password: string };
+	data: { id: number; password: string };
 }
+export const loginRequest = (data: {
+	id: number;
+	password: string;
+}): LoginRequestAction => {
+	return {
+		type: LOGIN_REQUEST,
+		data,
+	};
+};
 
 export interface LoginSuccessAction {
 	type: typeof LOGIN_SUCCESS;
-	data: { userId: string; nickname: string };
+	data: { userId: number; nickname: string };
 }
+export const loginSuccess = (data: {
+	userId: number;
+	nickname: string;
+}): LoginSuccessAction => {
+	return {
+		type: LOGIN_SUCCESS,
+		data,
+	};
+};
 
 export interface LoginFailureAction {
 	type: typeof LOGIN_FAILURE;
-	error: Error;
+	error: unknown;
 }
+export const loginFailure = (error: unknown): LoginFailureAction => {
+	return {
+		type: LOGIN_FAILURE,
+		error,
+	};
+};
 
-export const login = (data: { id: string; password: string }) => {};
+interface ThunkDispatch {
+	(thunkAction: ThunckAction): void;
+	<A>(action: A): A;
+	<TAction>(action: TAction | ThunckAction): TAction;
+}
+type ThunckAction = (dispatch: ThunkDispatch) => void;
+export const login = (data: { id: number; password: string }): ThunckAction => {
+	// 하나의 액션에 대한 dispatch를 수행하며 동시에 여러 액션의 dispatch를 수행한다. => thunk를 만든 이유
+	return (dispatch) => {
+		try {
+			dispatch(loginRequest(data));
+			setTimeout(() => {
+				dispatch(
+					loginSuccess({
+						userId: 1,
+						nickname: 'kyomin',
+					})
+				);
+				dispatch(addPost(''));
+			}, 1000);
+		} catch (err) {
+			dispatch(loginFailure(err));
+		}
+	};
+};
 
 export interface LogoutAction {
 	type: typeof LOGOUT;
 }
-
 export const logout = () => {
 	return {
 		type: LOGOUT,
--- 리덕스/store.ts
+++ 리덕스/store.ts
@@ -1,4 +1,12 @@
-import { createStore } from 'redux';
+import {
+	createStore,
+	MiddlewareAPI,
+	Dispatch,
+	AnyAction,
+	applyMiddleware,
+	compose,
+} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from './reducers';
 
 const initialState = {
@@ -9,6 +17,30 @@ const initialState = {
 	posts: [],
 };
 
-const store = createStore(reducer, initialState);
+const firstMiddleware =
+	(store: MiddlewareAPI) =>
+	(next: Dispatch<AnyAction>) =>
+	(action: AnyAction) => {
+		console.log('logging action : ', action);
+		next(action);
+	};
+
+const thunkMiddleware =
+	(store: MiddlewareAPI) => (next: Dispatch<AnyAction>) => (action: any) => {
+		if (typeof action === 'function') {
+			// 비동기
+			return action(store.dispatch, store.getState);
+		}
+
+		return next(action); // 동기
+	};
+
+// 미들웨어 장착!
+const enhancer =
+	process.env.NODE_ENV === 'production'
+		? compose(applyMiddleware(firstMiddleware, thunkMiddleware))
+		: composeWithDevTools(applyMiddleware(firstMiddleware, thunkMiddleware));
+
+const store = createStore(reducer, initialState, enhancer);
 
 export default store;
